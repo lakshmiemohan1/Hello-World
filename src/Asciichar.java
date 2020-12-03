fff65376396aef7de998d8ec4a27bc7f426630de
lakshmiemohan1 <lakshmiemohan1@gmail.com>
2020-12-03 13:41:32 -0500

    update in Asciichar.java

--- src/Asciichar.java
+++ src/Asciichar.java
@@ -4,6 +4,7 @@ public class Asciichar
   public static void printNumbers()
   {
 	  System.out.println("5Numbers!");
+	  System.out.println ("hi this is java program");
   }
 
   public static void printLowerCase()
